eb665fe6b731ef8fcb2ab0ec026dc64564e316f9
Maximiliano Jankovics <maxijankovics@gmail.com>
2024-12-05 17:57:55 -0300

    segundo commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
-import ChatManager from './chatManager.js';
+import ChatManager from './src/managers/chatManager.js';
 
 const app = express();
 const httpServer = createServer(app);
